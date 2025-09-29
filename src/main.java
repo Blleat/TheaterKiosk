6bd89933831a7390a43703e5365b623044850af8
Roshan <karkuroshan7@gmail.com>
2025-09-29 14:35:26 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        System.out.print("Enter your age: ");
+        age = in.nextInt();
+
+        if (age >= 21) {
+            System.out.println("You are 21 or older, you get a wristband");
+        }
+
+
+    }
+}
